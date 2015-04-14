e746582dcfec5d3c71aa2ccd50ca78292c4479e2
Denis I <visible@jumph4x.net>
2015-04-13 21:21:06 -0700

    More bootstrap

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-
+$('body').vegas({
+  slides: [
+    { src: '/img/1.jpg' },
+    { src: '/img/2.jpg' },
+    { src: '/img/3.jpg' },
+    { src: '/img/4.jpg' },
+    { src: '/img/5.jpg' },
+    { src: '/img/6.jpg' },
+    { src: '/img/7.jpg' },
+    { src: '/img/8.jpg' },
+    { src: '/img/9.jpg' },
+    { src: '/img/10.jpg' },
+    { src: '/img/11.jpg' },
+    { src: '/img/12.jpg' }
+  ]
+});
